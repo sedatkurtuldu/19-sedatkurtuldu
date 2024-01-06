8a77eb9fcc77ba40ea259b79a6ec128401ee356b
sedatkurtuldu <115918615+sedatkurtuldu@users.noreply.github.com>
2024-01-06 13:10:20 +0300

    .

--- src/app/_components/Product.tsx
+++ src/app/_components/Product.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import Image from 'next/image';
 import React from 'react'
 
